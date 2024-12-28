85252cef6d47035378014765c567970956132f34
fpaulaf02 <paulinhaf2002@gmail.com>
2024-12-28 18:07:10 +0000

    Update places.ts

--- data/places.ts
+++ data/places.ts
@@ -11,4 +11,14 @@ export const PLACES: IPin[] = [
     date: '2024-04-22',
     photo: '/photos/davidsousaa/salamanca.jpg'
   },
+  {
+    author: 'Alpaumarianal',
+    username: 'paulaferreira',
+    type: EPinType.Special,
+    city: 'Madeira',
+    country: 'Portugal',
+    coordinates: [32.6511, -16.9097],
+    date: '2024-07-10',
+    photo: '/photos/davidsousaa/madeira.jpg'
+  },
 ];
