2b30c3b707b87bdf3fe359322aa4baf075b9f6ac
fpaulaf02 <paulinhaf2002@gmail.com>
2024-12-28 17:59:41 +0000

    Update places.ts

--- data/places.ts
+++ data/places.ts
@@ -2,83 +2,13 @@ import { IPin, EPinType } from '~/lib/types';
 
 export const PLACES: IPin[] = [
   {
-    author: 'David Sousa, Filipe Tavares, Carolina Wilson, outros',
-    username: 'davidsousaa',
+    author: 'Alpaumarianal',
+    username: 'paulaferreira',
     type: EPinType.Special,
-    city: 'Lisboa',
-    country: 'Portugal',
-    coordinates: [38.7513889, -9.156472],
-    date: '2024-11-02',
+    city: 'Salamanca',
+    country: 'Espanha',
+    coordinates: [40.965, -5.66305],
+    date: '2024-04-22',
     photo: '/photos/davidsousaa/JENCVI.jpg'
-  },
-  {
-    author: 'Ana Martins, Ana Maçães, Beatriz Nunes, Bruno Lopes, Cristina Carneiro, Francisca Lopes, Guilherme Fernandes, João Dias, Xavier Rodrigues',
-    username: 'davidsousaa',
-    type: EPinType.Special,
-    city: 'Guimarães',
-    country: 'Portugal',
-    coordinates: [41.453602, -8.288644],
-    date: '2018-08-12',
-    photo: '/photos/davidsousaa/Founders.JPG'
-  },
-  {
-    author: 'Margarida Felgueiras, Sofia Gonçalves, Paula Ferreira, Simão Dias, Nelson Martins, Vítor Alves, Inês Faria',
-    username: 'davidsousaa',
-    type: EPinType.Special,
-    city: 'Porto',
-    country: 'Portugal',
-    coordinates: [41.148309, -8.660441],
-    date: '2023-09-17',
-    photo: '/photos/davidsousaa/ExBo2324.JPG'
-  },
-  {
-    author: 'Margarida Felgueiras, Sofia Gonçalves, Nelson Martins, Inês Faria, Madalena Passos, Rodrigo Ralha, Filipe Tavares, Carolina Wilson, Claúdia Lopes, David Sousa, Diana Fiúza, José Araújo, David Fernandes, João Gil, Francisco Gomes, Simão Pinheiro, José Alves, João Guimarães',
-    username: 'davidsousaa',
-    type: EPinType.Special,
-    city: 'Lisboa',
-    country: 'Portugal',
-    coordinates: [38.677938, -9.324846],
-    date: '2024-06-02',
-    photo: '/photos/davidsousaa/JENCV.jpg'
-  },
-  {
-    author: 'Margarida Felgueiras, Eduardo Barbosa, Paula Ferreira, Inês Faria ',
-    username: 'davidsousaa',
-    type: EPinType.Special,
-    city: 'Coimbra',
-    country: 'Portugal',
-    coordinates: [40.214747, -8.409342],
-    date: '2024-06-04',
-    photo: '/photos/davidsousaa/JeniAL2324.jpg'
-  },
-  {
-    author: 'Margarida Felgueiras, Tomás Silva, José Araújo, Inês Faria ',
-    username: 'davidsousaa',
-    type: EPinType.Special,
-    city: 'Bruxelas',
-    country: 'Bélgica',
-    coordinates: [50.838333, 4.408869],
-    date: '2024-04-03',
-    photo: '/photos/davidsousaa/JEEWC2024.jpeg'
-  },
-  {
-    author: 'Margarida Felgueiras, Paula Ferreira, Nelson Martins, Inês Faria, Sofia Gonçalves, Gabriela Gonçalves, Filipe Tavares, Eduardo Barbosa, Margarida Correia',
-    username: 'davidsousaa',
-    type: EPinType.Special,
-    city: 'Milão',
-    country: 'Itália',
-    coordinates: [45.461990, 9.177132],
-    date: '2023-11-11',
-    photo: '/photos/davidsousaa/JEEAC2023.jpeg'
-  },
-  {
-    author: 'Francisco Gomes, Filipe Tavares, Diana Fiúza',
-    username: 'davidsousaa',
-    type: EPinType.Special,
-    city: 'Bucareste',
-    country: 'Roménia',
-    coordinates: [44.44738, 26.09787],
-    date: '2024-11-09',
-    photo: '/photos/davidsousaa/JEEAC2024.jpg'
   },
 ];
