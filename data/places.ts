8fe8a5e98611e128150acc506c7c74966af58963
fpaulaf02 <paulinhaf2002@gmail.com>
2024-12-28 18:40:13 +0000

    Update places.ts

--- data/places.ts
+++ data/places.ts
@@ -9,7 +9,7 @@ export const PLACES: IPin[] = [
     country: 'Espanha',
     coordinates: [40.965, -5.66305],
     date: '2024-04-22',
-    photo: '/photos/davidsousaa/salamanca.jpg'
+    photo: '/photos/davidsousaa/Salamanca.jpg'
   },
   {
     author: 'Alpaumarianal',
@@ -20,5 +20,35 @@ export const PLACES: IPin[] = [
     coordinates: [32.6511, -16.9097],
     date: '2024-07-10',
     photo: '/photos/davidsousaa/madeira.jpg'
+  },
+  {
+    author: 'Alpaumarianal',
+    username: 'paulaferreira',
+    type: EPinType.Special,
+    city: 'Verona',
+    country: 'Itália',
+    coordinates: [45.4384, 10.9916],
+    date: '2022-06-30',
+    photo: '/photos/davidsousaa/verona.jpg'
+  },
+  {
+    author: 'Alpaumarianal',
+    username: 'paulaferreira',
+    type: EPinType.Special,
+    city: 'Bolonha',
+    country: 'Itália',
+    coordinates: [44.4949, 11.3426],
+    date: '2022-07-02',
+    photo: '/photos/davidsousaa/bolonha.jpg'
+  },
+  {
+    author: 'Alpaumarianal',
+    username: 'paulaferreira',
+    type: EPinType.Special,
+    city: 'Frioul, Marselha',
+    country: 'França',
+    coordinates: [43.2805, 5.3119],
+    date: '2023-07-03',
+    photo: '/photos/davidsousaa/marselha.jpg'
   },
 ];
